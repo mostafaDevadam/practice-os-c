dfce3ea94f2b95fcc967a7ff7801bc5832d80499
mostafaDevadam <adam.mostafa@outlook.de>
2026-07-27 08:03:45 +0200

    upload shell code

--- src/shell.c
+++ src/shell.c
@@ -1,6 +1,87 @@
 #include <stdio.h>
 #include "shell.h"
 
+
+void startMemoryManagement() {
+    printf("Starting memory management...\n");
+}
+
+void startCPUScheduler() {
+    printf("Starting CPU scheduler...\n");
+}
+
+void startDeadlockAvoidance() {
+    printf("Starting deadlock avoidance...\n");
+}
+
+void startConcurrency() {
+    printf("Starting concurrency...\n");
+}
+
+
+void startMultiCPUScheduler() {
+    
+}
+
+void shell_memory() {
+    
+}
+
+void shell_cpu_scheduler() {
+    
+}
+
+void shell_deadlock_avoidance() {
+    
+}
+
+void shell_concurrency() {
+    
+}
+
+void shell_multi_cpu_scheduler() {
+    
+}
+
 void startShell() {
     printf("Starting shell...\n");
+
+    int main_choice, memory_choice, cpu_choice, scheduler_choice, deadlock_choice, concurrency_choice, exit = 0;
+
+    while (!exit) {
+        printf("Main Menu:\n");
+        printf("1. Memory Management\n");
+        printf("2. CPU Scheduler\n");
+        printf("3. Deadlock Avoidance\n");
+        printf("4. Concurrency\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &main_choice);
+
+        switch (main_choice) {
+            case 1:
+                shell_memory();
+                break;
+            case 2:
+                shell_cpu_scheduler();
+                break;
+            case 3:
+                shell_deadlock_avoidance();
+                break;
+            case 4:
+                shell_concurrency();
+                break;
+            
+            case 5:
+                shell_multi_cpu_scheduler();
+                break;
+
+            case 6:
+                exit = 1;
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
+                break;
+        }
+    }
 }
