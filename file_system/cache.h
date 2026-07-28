6c0344be8cbae312f958d737f22c2ae85244744e
mostafaDevadam <adam.mostafa@outlook.de>
2026-07-29 01:23:07 +0200

    remove space

--- file_system/cache.h
+++ file_system/cache.h
@@ -1,29 +1,28 @@
- 
- #ifndef CACHE_H
- #define CACHE_H
 
- #include <stdint.h>
+#ifndef CACHE_H
+#define CACHE_H
+
+#include <stdint.h>
 #include "disk.h"
 
- #define CACHE_SIZE 64
+#define CACHE_SIZE 64
 
- typedef struct {
-    uint32_t block;
-    int valid;
-    int dirty;
-    uint32_t age;
-    char data[BLOCK_SIZE];
+typedef struct
+{
+   uint32_t block;
+   int valid;
+   int dirty;
+   uint32_t age;
+   char data[BLOCK_SIZE];
 
- } CacheEntry;
+} CacheEntry;
 
+int cache_init(void);
 
- int cache_init(void);
+int cache_read(uint32_t block, void *buffer);
 
- int cache_read(uint32_t block, void *buffer);
+int cache_write(uint32_t block, const void *buffer);
 
- int cache_write(uint32_t block, const void *buffer);
+void cache_flush(void);
 
- void cache_flush(void);
-
-
- #endif
+#endif
